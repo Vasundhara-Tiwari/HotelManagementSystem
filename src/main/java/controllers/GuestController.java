7e1cb66d368e26d21ae7162544b0390bacb9a87a
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-03-20 22:35:28 +0530

    creating guest controller

--- src/main/java/controllers/GuestController.java
+++ src/main/java/controllers/GuestController.java
@@ -1,5 +1,13 @@
 package controllers;
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import models.Guest;
+import ninja.jpa.UnitOfWork;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
 /**
  * Created by Vasundhara Tiwari
  * On 19-03-2022 at 00:08
@@ -7,4 +15,35 @@ package controllers;
 
 
 public class GuestController {
+
+    @Inject
+    Provider<EntityManager> entityManagerProvider;
+
+    @UnitOfWork
+    public boolean isUserAndPasswordValid(String username, String password) {
+
+        if (username != null && password != null) {
+
+            EntityManager entityManager = entityManagerProvider.get();
+
+            TypedQuery<Guest> q = entityManager.createQuery("SELECT x FROM Guest x WHERE username = :usernameParam", Guest.class);
+            Guest guest = getSingleResult(q.setParameter("usernameParam", username));
+            if (guest != null) {
+                if (guest.getPassword().equals(password)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static <T> T getSingleResult(TypedQuery<T> query) {
+        query.setMaxResults(1);
+        List<T> list = query.getResultList();
+        if (list == null || list.isEmpty()) {
+            return null;
+        }
+
+        return list.get(0);
+    }
 }
