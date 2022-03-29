9f55347618e7cbf3871264669b324ecfb4606d79
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-03-30 01:10:19 +0530

    Updating guest model

--- src/main/java/models/Guest.java
+++ src/main/java/models/Guest.java
@@ -14,16 +14,16 @@ public class Guest {
     @Id
     private int id;
     private String name;
-    private String username;
+    private String email;
     private String password;
 
     public Guest() {
     }
 
-    public Guest(int id, String name, String username, String password) {
+    public Guest(int id, String name, String email, String password) {
         this.id = id;
         this.name = name;
-        this.username = username;
+        this.email = email;
         this.password = password;
     }
 
@@ -43,12 +43,12 @@ public class Guest {
         this.name = name;
     }
 
-    public String getUsername() {
-        return username;
+    public String getEmail() {
+        return email;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPassword() {
