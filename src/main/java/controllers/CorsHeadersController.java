a783b034e7be0ab38f04f33ef69deae2c2853976
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-04-01 01:07:29 +0530

    creating cors headers controllers

---
+++ src/main/java/controllers/CorsHeadersController.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import com.google.inject.Inject;
+import ninja.Result;
+import ninja.Results;
+import ninja.utils.NinjaProperties;
+
+/**
+ * Created by Vasundhara Tiwari
+ * On 31-03-2022 at 10:48
+ */
+
+
+public class CorsHeadersController {
+
+    @Inject
+    NinjaProperties ninjaProperties;
+
+    public Result routeForOptions() {
+        System.out.println("------");
+        return addHeaders(Results.ok().json().render("key", "value"));
+    }
+    public Result addHeaders(Result result) {
+        return result.addHeader("Access-Control-Allow-Origin", "http://localhost:4200")
+                .addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS")
+                .addHeader("Access-Control-Request-Method", "GET, POST, DELETE, PUT, OPTIONS")
+                .addHeader("Access-Control-Allow-Headers", "apikey, hrEmail, Content-Type, Content-Range, Content-Disposition, Content-Description, Authorization, X-PING, Set-Cookie")
+                .addHeader("Access-Control-Request-Headers", "Cookie")
+                .addHeader("Access-Control-Max-Age", "85000");
+    }
+}
