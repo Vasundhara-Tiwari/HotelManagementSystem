fa773d87b5286c8efddd4d47d4be708ce766d085
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2022-04-08 16:34:41 +0530

    updating origin url

--- src/main/java/controllers/CorsHeadersController.java
+++ src/main/java/controllers/CorsHeadersController.java
@@ -25,6 +25,7 @@ public class CorsHeadersController {
                 .addHeader("Access-Control-Request-Method", "GET, POST, DELETE, PUT, OPTIONS")
                 .addHeader("Access-Control-Allow-Headers", "apikey, hrEmail, Content-Type, Content-Range, Content-Disposition, Content-Description, Authorization, X-PING, Set-Cookie")
                 .addHeader("Access-Control-Request-Headers", "Cookie")
-                .addHeader("Access-Control-Max-Age", "85000");
+                .addHeader("Access-Control-Max-Age", "85000")
+                .addHeader("Access-Control-Allow-Credentials", "true");
     }
 }
